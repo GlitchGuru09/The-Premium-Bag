4b4bba42ac37cc2c8bc4f61c8fecaf66c9e93b18
shrey vernekar <glitchguru03@gmail.com>
2025-01-12 11:12:14 +0530

    created route based on env variables

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose');
+const config = require('config');
+
+const dbgr = require('debug')('development:mongoose');
 
 mongoose
-.connect("mongodb://127.0.0.1:27017/thePremiumBag")
+.connect(`${config.get("MONGODB_URI")}/thePremiumBag`)
 .then(function(){
-    console.log("Mongodb server connected succesfully!");
+    dbgr("Mongodb server connected succesfully!");
 })
 .catch(function(err){
-    console.log(err);
+    dbgr(err);
 })
 
 module.exports = mongoose.connection;
+
+//to set env variable on local system using cmd "set NODE_ENV=development or export NODE_ENV=development"
+//export DEBUG=development:*
+// The DEBUG environment variable is used to enable debugging logs in Node.js applications that utilize the debug library.
+// The value development:* specifies which debug logs to enable. In this case, it enables all debug messages under the namespace starting with development:.
+
+//export NODE_ENV=development
+// The NODE_ENV environment variable is used to define the runtime environment of the Node.js application (e.g., development, production, or test).
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require('../models/owner-model');
 
 router.get('/',function(req,res){
     res.send("hey its working.");
 })
+
+// console.log(process.env.NODE_ENV);
+//creating routes based on env variable if the env variable i.e NODE_ENV != development then it wont show the create route
+if(process.env.NODE_ENV === "development"){
+    router.get('/create',function(req,res){
+        res.send("hey its working.");
+    })
+}
+
 
 module.exports = router;
