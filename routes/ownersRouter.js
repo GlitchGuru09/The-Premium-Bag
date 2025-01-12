4a76fc4f85eaa03a74dd60ffa06104974c884168
shrey vernekar <glitchguru03@gmail.com>
2025-01-12 11:39:12 +0530

    added check to create only one owner

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -7,10 +7,24 @@ router.get('/',function(req,res){
 })
 
 // console.log(process.env.NODE_ENV);
+// console.log(process.env.DEBUG);
+
 //creating routes based on env variable if the env variable i.e NODE_ENV != development then it wont show the create route
 if(process.env.NODE_ENV === "development"){
-    router.get('/create',function(req,res){
-        res.send("hey its working.");
+    router.get('/create',async function(req,res){
+        let owners = await ownerModel.find();
+        if(owners.length > 0){
+            return res
+                .status(403)
+                .send("You dont have permission to create new account");
+        }
+        let {fullname, email, password} = req.body;
+        let createdOwner = await ownerModel.create({
+            fullname,
+            email,
+            password
+        })
+        res.status(201).send(createdOwner);
     })
 }
 
